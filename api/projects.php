ba1bd493a998e9b8b1b220c5122f08ef4906fc66
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-16 19:18:56 +1100

    #014 Change textual mistakes

--- api/projects.php
+++ api/projects.php
@@ -14,7 +14,7 @@
             <h3>Current projects</h3>
             <p>I’m working on a project to classify trauma severity using EEG and other physiological data, with a dataset of 3,600 patients and 14 variables. I’ve optimized the model using a RandomForestRegressor. For me the goal of this project is not getting a super accurate model. 
                 Since there are better classyfing techinques for that. But to sharpen my mathematical understanding of simple 
-                regrssion and classyfing techinques. 
+                regression and classyfing techinques. 
         .</div>
             <p> I'm also at the beginning of the development of a spike sorting classifiers for long recordings with 
                 multi electrodes (neuropixel) recordings. <p. 
