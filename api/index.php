50e6a0cbe4e9a36b2373fc6039ed0b76db3885ea
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-17 15:17:15 +1100

    #026 try floating

--- api/index.php
+++ api/index.php
@@ -1,12 +1,10 @@
-<?php include 'header.php'; ?>
 <main>
-    <section class="intro">
+  <section class="intro">
+    <div class="intro-content">
+      <h1>Casper David van Laar</h1>
+      <p>Welcome to my portfolio. I'm an interdisciplinary student in AI and neuroscience, with a passion for understanding consciousness.</p>
+      <a href="/about">Learn more about me</a>
+    </div>
     <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
-            <div class="intro-content">
-           <h1>Casper David van Laar</h1>
-            <p>Welcome to my portfolio. I'm an interdisciplinary student in AI and neuroscience, with a passion for understanding consciousness.</p>
-            <a href="/about">Learn more about me</a>
-        </div>
-    </section>
+  </section>
 </main>
-<?php include 'footer.php'; ?>
