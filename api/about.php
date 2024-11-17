414d27d7b688318fc72f51bf26f71dcef2c348d6
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-17 14:04:31 +1100

    #024 Try order: 2 in styles

--- api/about.php
+++ api/about.php
@@ -3,10 +3,14 @@
     <section class="about">
         <h2>About Me</h2>
         <p>
-            I’m Casper David van Laar, an interdisciplinary student with a strong focus on the intersection of AI, neuroscience, and consciousness studies. 
-            My academic journey, from a Bachelor’s in Sport Science at HAN University in the Netherlands to a Master’s in Artificial Intelligence at the University of Wolverhampton, 
-            and a two year intership in a experimental and behavioural neuroscience lab has equipped me with a unique blend of skills in both cognitive science and computational modeling.
-            This foundation allows me to explore the complex relationships between brain function, behavior, and machine learning.
+            I’m Casper David van Laar, an Msc student in computer science and AI with a strong focus on the intersection of AI, neuroscience, and consciousness studies. 
+            My academic journey began as a bachelor in Sport Science at HAN University in the Netherlands to a Master’s. 
+            Where I discovered my love for statistics, programming and neuroscience.
+            During my last two years at the HAN I had the privilege of joining the department of neuroscience at Erasmus MC at the cerebro-cerebellar Communication lab as an intern.
+            In those two years of experimental and behavioural neuroscience I was taught the ins and outs of 
+            experimental neuroscience and programming data analytic pipelines. 
+            Aftwards I made the decision to pursue my master degree in computer science and Artificial Intelligence at the University of Wolverhampton
+            This foundation allows me to explore the complex relationships between brain function and behavior with machine learning.
         <p>
         <p> 
             My current research revolves around EEG signal analysis and the development of machine learning algorithms for neural data classification. 
@@ -31,7 +35,7 @@
             <li>Interdisciplinary Research in Neuroscience</li>
         </ul>
         <h3>Hobbies</h3>
-        <p>I enjoy Brazilian Jiu-Jitsu, theoretical physics, and exploring the philosophy of mind.</p>
+        <p>I enjoy Brazilian Jiu-Jitsu, science fiction, pop science books about theoretical physics and biology, and exploring the philosophy of mind.</p>
     </section>
 </main>
 <?php include 'footer.php'; ?>
