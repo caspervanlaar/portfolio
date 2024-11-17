d1607c3cc725cd4fa4ac467678393c44bfa2eeaa
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-17 15:13:04 +1100

    #025 Try img below intro instead of below intro-content

--- api/index.php
+++ api/index.php
@@ -1,9 +1,9 @@
 <?php include 'header.php'; ?>
 <main>
     <section class="intro">
-        <div class="intro-content">
-            <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
-            <h1>Casper David van Laar</h1>
+    <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
+            <div class="intro-content">
+           <h1>Casper David van Laar</h1>
             <p>Welcome to my portfolio. I'm an interdisciplinary student in AI and neuroscience, with a passion for understanding consciousness.</p>
             <a href="/about">Learn more about me</a>
         </div>
