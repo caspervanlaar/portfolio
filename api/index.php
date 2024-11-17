b8ea98cf1a3883100851abfffbc4bdc35d41ed9b
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-17 15:22:11 +1100

    #027 unfloat

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
       <h1>Casper David van Laar</h1>
       <p>Welcome to my portfolio. I'm an interdisciplinary student in AI and neuroscience, with a passion for understanding consciousness.</p>
       <a href="/about">Learn more about me</a>
+      <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
     </div>
-    <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
   </section>
 </main>
