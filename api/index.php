f1eec323b94295e2a8d1b7db1765d9b6df5aac7e
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-17 15:29:37 +1100

    #030 Textual changes + image size changes

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
     <div class="intro-content">
       <div class="text-content">
         <h1>Casper David van Laar</h1>
-        <p>Welcome to my portfolio. I'm an interdisciplinary student in AI and neuroscience, with a passion for understanding consciousness.</p>
+        <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p>
         <a href="/about">Learn more about me</a>
       </div>
       <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
