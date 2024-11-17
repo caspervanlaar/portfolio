ac1b1079353ab18af4eee6015f1e5ed4e0131fa7
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-17 15:38:46 +1100

    #031 Experience page added images. Styles allowing images

--- api/experience.php
+++ api/experience.php
@@ -1,29 +1,76 @@
 <?php include 'header.php'; ?>
 <main>
-    <section class="experience">
-        <h2>Experience</h2>
+  <section class="experience">
+    <h2>Experience</h2>
 
-        <!-- Erasmus MC Internship -->
-        <div class="experience-item">
-            <h3>Erasmus MC - Neuroscience Department</h3>
-            <p><strong>Intern</strong></p>
-            <p>During my internship at Erasmus MC in the Neuroscience Department, I focused on decoding electrophysiological data from deep cerebral nuclei and the medial prefrontal cortex (mPFC) to explore neural mechanisms underlying behavior and motor control. A significant portion of my work involved using advanced machine learning techniques to analyze the data obtained from these regions of the brain, ultimately contributing to our understanding of how the brain coordinates complex behaviors.</p>
+    <div class="experience-item">
+      <h3>Erasmus MC - Neuroscience Department</h3>
+      <p><strong>Intern</strong></p>
+      <p>
+        During my internship at Erasmus MC in the Neuroscience Department,
+        I focused on decoding electrophysiological data from deep cerebral nuclei
+        and the medial prefrontal cortex (mPFC) to explore neural mechanisms
+        underlying behavior and motor control. A significant portion of my work
+        involved using advanced machine learning techniques to analyze the data
+        obtained from these regions of the brain, ultimately contributing to our
+        understanding of how the brain coordinates complex behaviors.
+      </p>
+      <p>
+        I developed and implemented data analysis pipelines, which allowed for
+        better visualization and interpretation of neural activity. Specifically,
+        I conducted Principal Component Analysis (PCA) on mPFC neurons during
+        eyeblink conditioning tasks, aiming to identify and characterize the
+        dynamics of neural populations during behavioral learning. Additionally,
+        I worked on improving that pipeline with Demixed Principal Component
+        Analysis (DPCA) to assess neuronal activity in the deep cerebellum and
+        mPFC during a time-differentiating animal model experiment, which was
+        designed to enhance our ability to interpret neural responses.
+      </p>
+      <p>
+        In addition to my data analysis tasks, I was involved in several
+        experimental procedures, including training animals, performing
+        habituation sessions, conducting optostimulation experiments, and making
+        electrophysiological recordings in mice. This hands-on work not only
+        enhanced my technical skills but also deepened my understanding of how
+        these experimental techniques can be integrated with machine learning
+        models for more comprehensive data interpretation.
+      </p>
+      <p>
+        Throughout this internship, I also gained invaluable experience in
+        laboratory techniques such as stereotactic injections, transcardiac
+        perfusions, and brain slice histology. These skills were critical for
+        preparing neural tissue for further analysis and contributed to my
+        understanding of the biological foundations of behavior and
+        decision-making.
+      </p>
+      <img src="intr_27508536_1541176274.jpg" alt="Erasmus MC Internship - Neuroscience Department">
+    </div>
 
-            <p>I developed and implemented data analysis pipelines, which allowed for better visualization and interpretation of neural activity. Specifically, I conducted Principal Component Analysis (PCA) on mPFC neurons during eyeblink conditioning tasks, aiming to identify and characterize the dynamics of neural populations during behavioral learning. Additionally, I worked on improving that pipeline with Demixed Principal Component Analysis (DPCA) to assess neuronal activity in the deep cerebellum and mPFC during a time-differentiating animal model experiment, which was designed to enhance our ability to interpret neural responses in real-time.</p>
-
-            <p>In addition to my data analysis tasks, I was involved in several experimental procedures, including training animals, performing habituation sessions, conducting optostimulation experiments, and making electrophysiological recordings in mice. This hands-on work not only enhanced my technical skills but also deepened my understanding of how these experimental techniques can be integrated with machine learning models for more comprehensive data interpretation.</p>
-
-            <p>Throughout this internship, I also gained invaluable experience in laboratory techniques such as stereotactic injections, transcardiac perfusions, and brain slice histology. These skills were critical for preparing neural tissue for further analysis and contributed to my understanding of the biological foundations of behavior and decision-making.</p>
-        </div>
-
-        <!-- HAN Seneca Internship -->
-        <div class="experience-item">
-            <h3>HAN Seneca - Sport and Exercise Nutrition Research Team</h3>
-            <p><strong>Intern</strong></p>
-            <p>During my internship at HAN Seneca’s Sport and Exercise Nutrition Research Team, I was responsible for developing an e-learning module on sports nutrition aimed at improving athletes' understanding of nutrition science. The module provided evidence-based nutrition recommendations tailored to the specific needs of athletes, including former professional cyclists. In addition, I was involved in offering personalized nutrition and training advice to these athletes, ensuring that they received the most up-to-date and practical guidance to optimize their performance and recovery.</p>
-
-            <p>Another key responsibility was organizing and managing motor skills testing for children. This involved coordinating the logistics of the testing sessions, ensuring accurate data collection, and providing a supportive environment for participants. This experience not only enhanced my project management skills but also gave me valuable insights into how nutrition and physical activity influence cognitive and motor development in children.</p>
-        </div>
-    </section>
+    <div class="experience-item">
+      <h3>HAN Seneca - Sport and Exercise Nutrition Research Team</h3>
+      <p><strong>Intern</strong></p>
+      <p>
+        During my internship at HAN Seneca’s Sport and Exercise Nutrition
+        Research Team, I was responsible for developing an e-learning module on
+        sports nutrition aimed at improving athletes' understanding of
+        nutrition science. The module provided evidence-based nutrition
+        recommendations tailored to the specific needs of athletes, including
+        former professional cyclists. In addition, I was involved in offering
+        personalized nutrition and training advice to these athletes, ensuring
+        that they received the most up-to-date and practical guidance to
+        optimize their performance and recovery.
+      </p>
+      <p>
+        Another key responsibility was organizing and managing motor skills
+        testing for children. This involved coordinating the logistics of the
+        testing sessions, ensuring accurate data collection, and providing a
+        supportive environment for participants. This experience not only
+        enhanced my project management skills but also gave me valuable
+        insights into how nutrition and physical activity influence cognitive
+        and motor development in children.
+      </p>
+      <img src="https://s3.ap-south-1.amazonaws.com/leverageedu/school-logo/netherlands/2019-12-18_28_HAN-merkteken-descriptor.png" alt="HAN Seneca - Sport and Exercise Nutrition Research Team">
+    </div>
+  </section>
 </main>
 <?php include 'footer.php'; ?>
