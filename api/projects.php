c8a9cbe37f304ded7fedb96fa43af74ab465ff32
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-18 20:16:21 +1100

    #050 Chuck images in project page

--- api/projects.php
+++ api/projects.php
@@ -4,21 +4,24 @@
         <h2>Projects</h2>
         
         <div class="project">
-            <h3>EEG Signal Classification with Wavenet</h3>
-            <p>Developed advanced neural networks to classify EEG signals, achieving high accuracy and supporting decision-making research in neuroscience.</p>
-            <p><a href="https://github.com/caspervanlaar/EEG_wavenet" target="_blank">View on GitHub</a></p>
-        </div>     
+  <h3>EEG Signal Classification with Wavenet</h3>
+  <div class="project-content">
+    <img src="https://i.ibb.co/ygsx9SW/Schermafbeelding-2024-11-18-200146.png" alt="Schermafbeelding-2024-11-18-200146" border="0">
+    <p>Developed advanced neural networks to classify EEG signals, achieving high accuracy and supporting decision-making research in neuroscience.</p>
+    <p><a href="https://github.com/caspervanlaar/EEG_wavenet" target="_blank">View on GitHub</a></p>
+  </div>
+</div>     
         
 
-        <div class="project">
-            <h3>Current projects</h3>
-            <p>I’m working on a project to classify trauma severity using EEG and other physiological data, with a dataset of 3,600 patients and 14 variables. I’ve optimized the model using a RandomForestRegressor. For me the goal of this project is not getting a super accurate model. 
-                Since there are better classyfing techinques for that. But to sharpen my mathematical understanding of simple 
-                regression and classyfing techinques. 
-        .</div>
-            <p> I'm also at the beginning of the development of a spike sorting classifiers for long recordings with 
-                multi electrodes (neuropixel) recordings. <p. 
-        </div>
+
+<div class="project">
+  <h3>Current projects</h3>
+  <div class="project-content">
+  <a href="https://imgbb.com/"><img src="https://i.ibb.co/8sG8G0b/eq.png" alt="eq" border="0"></a>
+    <p>I’m working on a project to classify trauma severity using EEG and other physiological data, with a dataset of 3,600 patients and 14 variables. I’ve optimized the model using a RandomForestRegressor. For me the goal of this project is not getting a super accurate model.  Since there are better classifying techniques for that. But to sharpen my mathematical understanding of simple regression and classifying techniques. </p>
+    <p> I'm also at the beginning of the development of a spike sorting classifiers for long recordings with multi electrodes (neuropixel) recordings. </p>
+  </div>
+</div>
 
     </section>
 </main>
