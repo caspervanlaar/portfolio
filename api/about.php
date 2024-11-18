3dcb2ff9244d679b47d6beae4965dd7546e55f64
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-18 18:31:34 +1100

    #048 Change TXT and image to alllign with header...

--- api/about.php
+++ api/about.php
@@ -2,8 +2,11 @@
 <main>
     <section class="about">
         <h2>About Me</h2>
+        I enjoy <a href="https://www.instagram.com/p/CuQYPZjMjHo/">Brazilian Jiu-Jitsu</a>, reading 
+        <a href="https://www.goodreads.com/user/show/177170325-casper-david"> science fiction and non fiction books </a>. 
+    
         <p>
-            I’m Casper David van Laar, an Msc student in computer science and AI with a strong focus on the intersection of AI, neuroscience, and consciousness studies. 
+            I’m a Msc student in computer science and artificial intelligence with a strong focus on the intersection of AI, neuroscience, and consciousness studies. 
             My academic journey began as a bachelor in Sport Science at HAN University in the Netherlands to a Master’s. 
             Where I discovered my love for statistics, programming and neuroscience.
             During my last two years at the HAN I had the privilege of joining the department of neuroscience at Erasmus MC at the cerebro-cerebellar Communication lab as an intern.
@@ -13,7 +16,7 @@
             This foundation allows me to explore the complex relationships between brain function and behavior with machine learning.
         <p>
         <p> 
-            My current research revolves around EEG signal analysis and the development of machine learning algorithms for neural data classification. 
+            My current research revolves around EEG signal analysis and the development of machine learning algorithms and neural netoworks for neural data classification. 
             I’m particularly interested in understanding brain activity during behavioral tasks, as well as the neurophysiological underpinnings 
             of consciousness and decision-making. To advance this understanding, I’ve been working on building machine learning pipelines 
             capable of integrating electrophysiological data, such as EEG and Neuropixels recordings, with behavioral insights. Recently, 
@@ -22,7 +25,7 @@
         <p>
         <p>
             In addition to my research, I’m deeply committed to interdisciplinary exploration. 
-            I’m drawn to areas like theoretical physics and philosophy of mind, and I aspire to integrate AI, neuroscience, and mathematics to tackle fundamental questions about consciousness. 
+            I’m drawn to areas like (theoretical) physics and philosophy of mind, and I aspire to integrate AI, neuroscience, and mathematics to tackle fundamental questions about consciousness. 
             At the moment I am studying remotely from Australia and I’m eager to build collaborations in the neuroscience community 
             and contribute meaningfully to the field. 
             Ultimately, I aim to push the boundaries of AI and neuroscience to deepen our understanding of the mind.</p>
@@ -34,8 +37,9 @@
             <li>Machine Learning for EEG Analysis and Neuroscience</li>
             <li>Interdisciplinary Research in Neuroscience</li>
         </ul>
-        <h3>Hobbies</h3>
-        <p>I enjoy Brazilian Jiu-Jitsu, science fiction, pop science books about theoretical physics and biology, and exploring the philosophy of mind.</p>
-    </section>
+        
+<p>
+ 
+</p></section>
 </main>
 <?php include 'footer.php'; ?>
