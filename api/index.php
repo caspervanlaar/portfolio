32e3fd9a291587f657a11c5236f702a30e9dab5a
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-19 17:26:51 +1100

    #055 Back

--- api/index.php
+++ api/index.php
@@ -2,11 +2,11 @@
 <main>
   <section class="intro">
     <div class="intro-content">
-      
+      <div class="text-content">
         <h1>Casper David van Laar</h1>
         <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p>
         <a href="/about">Learn more about me</a>
-    
+      </div>
       <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
     </div>
   </section>
