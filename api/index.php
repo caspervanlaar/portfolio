5f233448f00e60f2381bba657848e808e79cb6fb
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-21 17:06:00 +1100

    #058 link back in box

--- api/index.php
+++ api/index.php
@@ -1,16 +1,16 @@
 <?php include 'header.php'; ?>
 <main>
   <section class="intro">
-    <div class="intro-content">      
-       
-        <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p>
-        
-        <div class="header-image">
+    < class="intro-content">      
+    <div class="header-image">
       <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
       <h1>Casper David van Laar</h1>
       </div>
+        <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p> </br>  
+        <a href="/about">Learn more about me</a>       
+        
     </div>
-    <a href="/about">Learn more about me</a>
+    
   </section>
 </main>
 <?php include 'footer.php'; ?>
