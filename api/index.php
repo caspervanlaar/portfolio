794f0a7726fcd9df280c8d18efb97071a27c130b
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-21 17:12:55 +1100

    #060 header and footer back

--- api/index.php
+++ api/index.php
@@ -1,3 +1,4 @@
+<?php include 'header.php'; ?>
 <section class="intro">
   <div class="intro-content">
     <div class="text-content">
@@ -8,3 +9,4 @@
     <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
   </div>
 </section>
+<?php include 'footer.php'; ?>
