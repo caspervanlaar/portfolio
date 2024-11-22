f60b582ac52165f18c448f73448bf99cf54a4753
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-22 17:12:57 +1100

    #067 resused experience class for index

--- api/index.php
+++ api/index.php
@@ -1,12 +1,16 @@
 <?php include 'header.php'; ?>
-<section class="intro">
-  <div class="intro-content">
-    <div class="text-content">
-      <h1>Casper David van Laar</h1>
-      <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p>
+<section class="experience">
+<div class="experience-container">
+    <div class="experience-item">
+        <div class="header-image">    
+        <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
+      <h1>Casper David van Laar</h1> 
+      </div>      
+      < class="text-content">  
+      <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p> </br>
       <a href="/about">Learn more about me</a>
-    </div>
-    <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
-  </div>
+      </div>  
+      </div>
+    
 </section>
 <?php include 'footer.php'; ?>
