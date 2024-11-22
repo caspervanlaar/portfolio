f2cf63d0e51440dcd3c98f9f00415f7b60c2d90c
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-22 18:04:19 +1100

    #078 AGAIN

--- api/index.php
+++ api/index.php
@@ -2,8 +2,11 @@
 <section class="intro">
 <div class="intro-container">
     <div class="intro-item">                    
-        <div class="header-image" style="display: flex; justify-content: space-between;">  <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
-            <h1>Casper David van Laar</h1>     
+        <div class="header-image" style="display: flex; justify-content: space-between; max-width: 150px;
+    height: auto;
+    border-radius: 50%;      
+    margin-right: 50px;">  <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
+            <h3>Casper David van Laar</h3>     
         </div>         
       <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p> </br>
       <a href="/about">Learn more about me</a>
