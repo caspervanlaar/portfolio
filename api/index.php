108e0fa774d12e52e7a17a14320f63813a0e9b6a
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-22 17:52:51 +1100

    #076 AGAIN

--- api/index.php
+++ api/index.php
@@ -2,14 +2,14 @@
 <section class="experience">
 <div class="intro-container">
     <div class="intro-item">
-    <h1>Casper David van Laar</h1> 
-        <div class="header_intro-image">        
+        <div class="header_intro-image">    
+        <h1>Casper David van Laar</h1> 
         <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
-            </div>         
-            <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p> </br>
-            <a href="/about">Learn more about me</a>
-            </div>  
-        </div>
+      </div>         
+      <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p> </br>
+      <a href="/about">Learn more about me</a>
+      </div>  
+      </div>
     
 </section>
 <?php include 'footer.php'; ?>
