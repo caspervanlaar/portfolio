1a82e30f69730201c5d29150aca14944c3b4b725
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-22 17:26:37 +1100

    #068 mirror picture and make circle. Intro

--- api/index.php
+++ api/index.php
@@ -2,11 +2,10 @@
 <section class="experience">
 <div class="experience-container">
     <div class="experience-item">
-        <div class="header-image">    
+        <div class="header_intro-image">    
         <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
       <h1>Casper David van Laar</h1> 
-      </div>      
-      < class="text-content">  
+      </div>         
       <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p> </br>
       <a href="/about">Learn more about me</a>
       </div>  
