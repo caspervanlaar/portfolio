77e1b0958848549feaec06a4e1068265710d0bde
caspervanlaar <spamcasperdvanlaar@example.nl>
2024-11-22 17:34:19 +1100

    #070 picture to the right

--- api/index.php
+++ api/index.php
@@ -1,10 +1,10 @@
 <?php include 'header.php'; ?>
 <section class="experience">
 <div class="experience-container">
-    <div class="experience-item">
+    <div class="intro-item">
         <div class="header_intro-image">    
+        <h1>Casper David van Laar</h1> 
         <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
-      <h1>Casper David van Laar</h1> 
       </div>         
       <p>Welcome to my portfolio. I'm a student in AI with a passion for neuroscience.</p> </br>
       <a href="/about">Learn more about me</a>
