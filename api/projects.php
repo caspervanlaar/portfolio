cb1c06163ed94db261f8d5abf4641ad42c037ad8
caspervanlaar <spamcasperdvanlaar@example.nl>
2025-05-26 12:28:41 +1000

    #086 About page update project

--- api/projects.php
+++ api/projects.php
@@ -3,11 +3,25 @@
     <section class="projects">
         <h2>Projects</h2>
         
+
+
+<div class="project">
+  <h3>Current project</h3>
+  <div class="project-content">
+  <a href="https://th.bing.com/th/id/R.193be8fa5e287fb9d9168a273fda135c?rik=2hMIMhE3XajihA&riu=http%3a%2f%2fwww.kurzweilai.net%2fimages%2fNeuropixels-probe.jpg&ehk=tOV7kDjQCUYFv1Jz2t8aT5kBskaQerPjJbR4T0t%2fqJQ%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1" target="_blank"><img src="https://th.bing.com/th/id/R.193be8fa5e287fb9d9168a273fda135c?rik=2hMIMhE3XajihA&riu=http%3a%2f%2fwww.kurzweilai.net%2fimages%2fNeuropixels-probe.jpg&ehk=tOV7kDjQCUYFv1Jz2t8aT5kBskaQerPjJbR4T0t%2fqJQ%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1" alt="Neuropixel Probe" border="0"></a>
+    <p>I’m currently at the early stages of my master thesis, developing a hybrid CNN-WaveNet model for classifying action 
+      potentials from Neuropixels recordings. The approach uses a Kilosort-inspired preprocessing pipeline 
+      and integrates explainability tools to better understand model decisions. The goal is to improve spike classification 
+      accuracy in noisy data and contribute to more interpretable neural decoding methods </p>
+  </div>
+</div>
+
         <div class="project">
   <h3>EEG Signal Classification with Wavenet</h3>
   <div class="project-content">
     <img src="https://i.ibb.co/ygsx9SW/Schermafbeelding-2024-11-18-200146.png" alt="Schermafbeelding-2024-11-18-200146" border="0">
-    <p>Developed advanced neural networks to classify EEG signals, achieving high accuracy and supporting decision-making research in neuroscience.</p>
+    <p>Developed advanced neural networks to classify EEG signals, achieving high accuracy and supporting decision-making 
+      research in neuroscience.</p>
     <p><a href="https://github.com/caspervanlaar/EEG_wavenet" target="_blank">View on GitHub</a></p>
   </div>
 </div>     
@@ -18,18 +32,22 @@
   <h3>Current projects</h3>
   <div class="project-content">
   <a href="https://imgbb.com/"><img src="https://i.ibb.co/8sG8G0b/eq.png" alt="eq" border="0"></a>
-    <p>I’m working on a project to classify trauma severity using EEG and other physiological data. Using LASSO - and random forrest regression.
+    <p>I have worked on a project to classify trauma severity using EEG and other physiological data. Using LASSO - and random forrest regression.
         The goal of this project was not getting a the best model.  Since there are better classifying techniques for that.
         But to sharpen my mathematical understanding of regression and classifying techniques. </p>
     </div>
 </div>
 
 
+
 <div class="project">
-  <h3>Current projects</h3>
+  <h3>EEG-Based Comprehension Prediction</h3>
   <div class="project-content">
-  <a href="https://th.bing.com/th/id/R.193be8fa5e287fb9d9168a273fda135c?rik=2hMIMhE3XajihA&riu=http%3a%2f%2fwww.kurzweilai.net%2fimages%2fNeuropixels-probe.jpg&ehk=tOV7kDjQCUYFv1Jz2t8aT5kBskaQerPjJbR4T0t%2fqJQ%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1" target="_blank"><img src="https://th.bing.com/th/id/R.193be8fa5e287fb9d9168a273fda135c?rik=2hMIMhE3XajihA&riu=http%3a%2f%2fwww.kurzweilai.net%2fimages%2fNeuropixels-probe.jpg&ehk=tOV7kDjQCUYFv1Jz2t8aT5kBskaQerPjJbR4T0t%2fqJQ%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1" alt="Neuropixel Probe" border="0"></a>
-    <p>I'm also at the beginning of the development of a spike sorting classifiers for long recordings with multi electrodes (neuropixel) recordings. </p>
+  <a href="https://imgbb.com/"><img src="https://i.ibb.co/HTyxvqBZ/Screenshot-2025-05-26-at-12-24-25-To-understand-or-not-to-understand-REPORT-CVL-2440678-EEG-7-CS074.png" alt="Screenshot-2025-05-26-at-12-24-25-To-understand-or-not-to-understand-REPORT-CVL-2440678-EEG-7-CS074-" border="0"></a>
+   <p>This project explored whether EEG signals can predict how well subjects understand educational videos. 
+      Using 14-channel EEG data and self-reported comprehension scores, I applied both supervised and unsupervised learning methods.
+      Support Vector Machines achieved up to 88.66% balanced accuracy. PCA and K-Means revealed underlying patterns 
+      linked to comprehension. The findings highlight EEG’s potential for adaptive learning systems and real-time cognitive monitoring. </p>
   </div>
 </div>
 
