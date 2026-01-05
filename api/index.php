a807b812290e2f6c246dc4d2c8a9d5838e48a04d
caspervanlaar <83173582+caspervanlaar@users.noreply.github.com>
2026-01-05 14:54:47 +1100

    Update profile image source in index.php

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
     <div class="intro-item">
         <div class="header_intro-image">    
         <h1>Casper David van Laar</h1> 
-        <img src="https://i.postimg.cc/xj4N1nDf/361052844-1225684538140720-6642090904425457264-n.jpg" alt="Profile Picture">
+        <img src="https://raw.githubusercontent.com/caspervanlaar/portfolio/refs/heads/main/me.jpg" alt="me">
       </div>         
       <p>Neuroscience researcher and AI practitioner exploring the brain through experimental and computational approaches. 
           I develop models and analysis pipelines to decode neural activity and understand learning, decision-making, and conscious processes.</p> </br>
