8f4ab754b56974bb4180e6853fc47d40f0aef88c
caspervanlaar <83173582+caspervanlaar@users.noreply.github.com>
2026-01-05 16:47:35 +1100

    Update thankyou.php

--- api/thankyou.php
+++ api/thankyou.php
@@ -2,8 +2,7 @@
 <main>
     <section class="project">
         <div class="project-body" style="text-align: center;">
-            <h2>Thank You!</h2>
-            <p style="font-size: 1.2rem; max-width: 700px; margin: 1.5rem auto;">
+            <h2>Thank You!</h2>            
                 Your message has been sent successfully. I’ll get back to you as soon as possible.
             </p>
             <a href="/" class="toggle-button" style="text-decoration:none;">
