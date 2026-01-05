f91d1939dfeb1638af56a29ef54a2abf439b5c9e
caspervanlaar <83173582+caspervanlaar@users.noreply.github.com>
2026-01-05 16:49:05 +1100

    Update thankyou.php

--- api/thankyou.php
+++ api/thankyou.php
@@ -1,14 +1,13 @@
 <?php include 'header.php'; ?>
 <main>
-    <section class="project">
-        <div class="project-body" style="text-align: center;">
+    <section class="projects">        
             <h2>Thank You!</h2>            
                 Your message has been sent successfully. I’ll get back to you as soon as possible.
             </p>
             <a href="/" class="toggle-button" style="text-decoration:none;">
                 Back to Home
             </a>
-        </div>
+        
     </section>
 </main>
 <?php include 'footer.php'; ?>
