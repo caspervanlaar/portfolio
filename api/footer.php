dd5af5eeaf30a864acdfd93f23b066976bfb3e03
caspervanlaar <83173582+caspervanlaar@users.noreply.github.com>
2026-01-08 16:05:41 +1100

    Update footer.php

--- api/footer.php
+++ api/footer.php
@@ -4,19 +4,22 @@
 
         <div class="footer-icons">
             <a href="https://github.com/caspervanlaar" target="_blank" aria-label="GitHub">
-                <i class="fa-brands fa-github"></i>
+                <i class="fab fa-github"></i>
             </a>
 
             <a href="https://www.linkedin.com/in/YOUR-LINKEDIN-HERE" target="_blank" aria-label="LinkedIn">
-                <i class="fa-brands fa-linkedin"></i>
+                <i class="fab fa-linkedin"></i>
             </a>
 
             <a href="mailto:casperdvanlaar@hotmail.com" aria-label="Email">
-                <i class="fa-solid fa-envelope"></i>
+                <i class="fas fa-envelope"></i>
             </a>
 
             <a href="https://orcid.org/0009-0002-2703-2216" target="_blank" aria-label="ORCID">
-                <i class="fa-brands fa-orcid"></i>
+                <!-- Inline ORCID SVG -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 256 256" fill="white">
+                    <path d="M128 0C57.31 0 0 57.31 0 128s57.31 128 128 128 128-57.31 128-128S198.69 0 128 0zm0 234.667c-58.94 0-106.667-47.727-106.667-106.667S69.06 21.333 128 21.333 234.667 69.06 234.667 128 186.94 234.667 128 234.667zm-44.267-128c0 24.467 19.8 44.267 44.267 44.267s44.267-19.8 44.267-44.267S152.467 62.4 128 62.4 83.733 102.2 83.733 106.667zm0 0"/>
+                </svg>
             </a>
         </div>
     </div>
