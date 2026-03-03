65f9641503cfa5810eccf6952bbec92571d002bf
caspervanlaar <83173582+caspervanlaar@users.noreply.github.com>
2026-03-03 22:42:58 +1100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,8 +6,8 @@
         <h1>Casper David van Laar</h1> 
         <img src="https://raw.githubusercontent.com/caspervanlaar/portfolio/refs/heads/main/me.jpg" alt="me">
       </div>         
-      <p>Neuroscience researcher and AI practitioner exploring the brain through experimental and computational approaches. 
-          I develop models and analysis pipelines to decode neural activity and understand learning, decision-making, and conscious processes.</p> </br>
+      <p>Neuroscience and AI researcher exploring the neural networks through experimental and computational approaches. 
+          I develop models and analysis pipelines to decode neural activity and understand learning.</p> </br>
       <a href="/about">Learn more about me</a>
       </div>  
       </div>
