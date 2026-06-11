58762ac8c1b3d7bb7909bdb6461acf30da2a8423
caspervanlaar <83173582+caspervanlaar@users.noreply.github.com>
2026-06-11 11:51:38 +1000

    Revise personal introduction in index.php
    Updated personal description to reflect current studies and focus.

--- api/index.php
+++ api/index.php
@@ -6,8 +6,8 @@
         <h1>Casper David van Laar</h1> 
         <img src="https://raw.githubusercontent.com/caspervanlaar/portfolio/refs/heads/main/me.jpg" alt="me">
       </div>         
-      <p>Neuroscience and AI researcher exploring the neural networks through experimental and computational approaches. 
-          I develop models and analysis pipelines to decode neural activity and understand learning.</p> </br>
+      <p>Msc Computer Science and AI researcher exploring biological and artificial neural networks through experimental and computational approaches.
+                        I develop models and analysis pipelines to decode neural activity and understand learning. </p> </br>
       <a href="/about">Learn more about me</a>
       </div>  
       </div>
